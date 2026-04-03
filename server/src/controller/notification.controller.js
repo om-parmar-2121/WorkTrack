0ce3efa9ef7e6c58f7dde2efb5be0899cf152900
Om Parmar <itsop.omparmar@gmail.com>
2026-04-03 07:10:20 +0530

    update title to StaffSphere

--- server/src/controller/notification.controller.js
+++ server/src/controller/notification.controller.js
@@ -1,6 +1,5 @@
 import Notification from "../models/notification.model.js";
 import { asyncHandler } from "../utilities/asyncHandler.utility.js";
-import { errorHandler } from "../utilities/errorHandler.utility.js";
 
 export const getMyNotifications = asyncHandler(async (req, res, next) => {
   const notifications = await Notification.find({ userId: req.user._id })
@@ -8,11 +7,8 @@ export const getMyNotifications = asyncHandler(async (req, res, next) => {
     .sort({ createdAt: -1 })
     .limit(5);
 
-  if (!notifications || notifications.length === 0)
-    return next(new errorHandler("No notifications found", 404));
-
   res.status(200).json({
     success: true,
-    notifications,
+    notifications: notifications || [],
   });
 });
